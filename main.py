ca1967c6e5ffd4f929420239603cba898bef8cb3
Sergey <syrykh.ser@gmail.com>
2022-04-12 23:21:06 +0700

    add testing code

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 def solution(number):
     if (type(number) == int):
-        return sum(x for x in range(number+1) if x % 3 == 0 or x % 5 == 0)
+        return sum(x for x in range(number + 1) if x % 3 == 0 or x % 5 == 0)
     else:
-        return("Иди опохмелись, брат")
+        return ("Иди опохмелись, брат")
+
+
+def is_int(str):
+    try:
+        int(str)
+        return True
+    except ValueError:
+        return False
+
+
+def reading_and_writing(number):
+    #file_1 = open('arg.txt') Существует для чтения из файла
+    #for line in file_1:
+    if is_int(number):
+        answer = solution(int(number))
+    else:
+        answer = "net net net"
+
+
+    file_2 = open('answers.txt', 'a')
+    file_2.write(f'{answer} for number - {number} \n')
+    file_2.close()
+
+    #file_1.close() Существует для чтения из файла
+    return 0
+
+
+reading()
---
+++ test_solution_and_file.py
@@ -0,0 +1,11 @@
+import pytest
+from main import solution
+
+def test_normal_second():
+    assert solution(142) == 4739
+
+
+
+
+if __name__ == '__main__':
+    pytest.main()
